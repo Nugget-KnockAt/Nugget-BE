792f93b74524d7ea867a550f24a26a67e85993fc
seonghwa95 <holyflower95@naver.com>
2024-01-24 00:01:56 +0900

    Refactor: Entity update

---
+++ src/main/java/com/example/nuggetbe/entity/Connection.java
@@ -0,0 +1,25 @@
+package com.example.nuggetbe.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+public class Connection {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "guardian_id")
+    private Guardian guardian;
+}
