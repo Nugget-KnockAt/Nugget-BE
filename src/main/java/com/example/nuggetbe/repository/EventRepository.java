1a46f17b02eaa1c05751147c91f2b8f4b86a767a
seonghwa95 <holyflower95@naver.com>
2024-02-08 15:39:00 +0900

    Feat: find All eventList recent 7Days

--- src/main/java/com/example/nuggetbe/repository/EventRepository.java
+++ src/main/java/com/example/nuggetbe/repository/EventRepository.java
@@ -1,7 +1,13 @@
 package com.example.nuggetbe.repository;
 
 import com.example.nuggetbe.entity.Event;
+import com.example.nuggetbe.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface EventRepository extends JpaRepository<Event, Long> {
+
+    List<Event> findAllByMemberAndCreatedAtAfter(Member member, LocalDateTime time);
 }
